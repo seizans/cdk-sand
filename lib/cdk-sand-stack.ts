94a7d96adbbd9f397b3f75d3a96b49bb5478ec8d
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2024-01-11 16:56:13 +0900

    Hello s3 bucket

--- lib/cdk-sand-stack.ts
+++ lib/cdk-sand-stack.ts
@@ -6,6 +6,8 @@ export class CdkSandStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new cdk.aws_s3.Bucket(this, 'MyFirstBucket', {versioned: true});
+
     // The code that defines your stack goes here
 
     // example resource
