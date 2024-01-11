187f5cf05e4a379d54ef29e22e745ada8c5202fe
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2024-01-11 16:36:55 +0900

    cdk init app --language typescript

---
+++ test/cdk-sand.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSand from '../lib/cdk-sand-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-sand-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSand.CdkSandStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
